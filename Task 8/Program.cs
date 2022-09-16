e6f52a332bb0d385152316790ae4bfdbaab3a5c9
Zazhiwihina Valeria <zazhiwihina@yandex.ru>
2022-09-16 15:55:02 +0400

    Задача 8

---
+++ Task 8/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.Write("Введите число: ");
+String number = Console.ReadLine();
+int num = int.Parse(number);
+
+if (num < 2)
+{
+    if (num >= 0)
+    {
+        Console.Write("Четных чисел нет");
+    }
+    else
+    {
+        Console.Write("Положительных чисел нет");
+    }
+}
+else
+{
+    for (int i = 2; i <= num; i = i + 2)
+    {
+        Console.WriteLine(i);
+    }
+}
